ee70ad06b2c2b7a36107ee70973bb0caaa03bd56
Youssef Ezz <yos_ezzat@hotmail.com>
2023-11-20 02:08:26 +0200

    add preprocessing of dataset 3 using jupiter notebook

--- MyNeuralNetwork.py
+++ MyNeuralNetwork.py
@@ -34,7 +34,14 @@ class MyNeuralNetwork:
   #feature vectors; and a vector y of size (n_samples), which holds the target
   #values for the training samples
   def fit(self, X, y):
-
+    num_training_patterns = X.shape[0]
+    for i in range(0 , num_training_patterns):
+      self.feedforward(X[i])
+    return 0
+  
+  #Feed−forward propagation of pattern xµ to obtain the output o(xµ)
+  def feedforward(self, x):
+    y = x
     return 0
 
 #read and parse the .csv features file 
@@ -43,20 +50,26 @@ df.head()
 
 columns = df.shape[1]
 
-# construct an array of arrays size (451, 4)
+# construct an array of arrays size (451, 4) for all features input values
 inputcolumns = df.columns[0 : 4]
 features = df[inputcolumns].values
 
-# construct an array of size (451)
+#select the first 85% as training features an array of arrays size (383, 4)
+num_training_features = int(85 * features.shape[0] / 100)
+training_features = features[0 : num_training_features]
+
+# construct an array of size (451) for all features target values
 outputcolumn = df.columns[4]
 targets = df[outputcolumn].values
+#select the first 85% as training tsrgets an array  size (383)
+training_targets = targets[0 : num_training_features]
 
 # layers include input layer + hidden layers + output layer
 layers = [4, 9, 5, 1]
 nn = MyNeuralNetwork(layers)
 
 # call fit function with features (n_samples,n_features) and targets (n_samples)
-nn.fit(features, targets)
+nn.fit(training_features, training_targets)
 
 print("L = ", nn.L, end="\n")
 print("n = ", nn.n, end="\n")
